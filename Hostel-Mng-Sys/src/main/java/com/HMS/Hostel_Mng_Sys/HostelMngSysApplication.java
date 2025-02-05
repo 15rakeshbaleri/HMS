96bd0f6db14aa75f8f69f5c421869b52e8132d27
rakesh baleri <rrbaleri@gmail.com>
2025-02-06 00:53:40 +0530

    first commit

---
+++ Hostel-Mng-Sys/src/main/java/com/HMS/Hostel_Mng_Sys/HostelMngSysApplication.java
@@ -0,0 +1,13 @@
+package com.HMS.Hostel_Mng_Sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostelMngSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostelMngSysApplication.class, args);
+	}
+
+}
---
+++ Hostel-Mng-Sys/src/test/java/com/HMS/Hostel_Mng_Sys/HostelMngSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HMS.Hostel_Mng_Sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostelMngSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
